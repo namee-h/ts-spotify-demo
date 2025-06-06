bfae9e9484fb7f4fc18c9d34a28ec252bdd0c39a
Sara han <hans63237@gmail.com>
2025-06-06 17:01:16 +0900

    임시저장

---
+++ src/layout/components/Playlists.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Playlists = ({ key, playlists }) => {
+  return <div>Playlists</div>;
+};
+
+export default Playlists;
--- src/theme.ts
+++ src/theme.ts
@@ -38,6 +38,15 @@ const theme = createTheme({
       fontSize: "0.6875rem",
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {
