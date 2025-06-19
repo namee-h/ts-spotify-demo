1f77bb6141f6f9d5cefcbc28f06c3c73f86bfd69
Sara han <hans63237@gmail.com>
2025-06-19 22:08:24 +0900

    스크롤숨기기

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,16 +4,27 @@ import OldTracks from "./components/OldTracks";
 import NewTracks from "./components/NewTracks";
 import HipsterAlbums from "./components/HipsterAlbums";
 import KPopArtists from "./components/KPopArtists";
+import { Box, styled } from "@mui/material";
+
+const ScrollContainer = styled(Box)({
+  maxHeight: "100dvh",
+  overflowY: "scroll",
+  scrollbarWidth: "none",
+  msOverflowStyle: "none",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+});
 
 const HomePage = () => {
   return (
-    <div style={{ maxHeight: "100dvh", overflowY: "auto" }}>
+    <ScrollContainer style={{ maxHeight: "100dvh", overflowY: "auto" }}>
       <NewReleases />
       <NewTracks />
       <KPopArtists />
       <OldTracks />
       <HipsterAlbums />
-    </div>
+    </ScrollContainer>
   );
 };
 
